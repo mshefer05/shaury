a2dc4d3d89fa868c285a32bc6e0d975cafa95609
mshefer05 <105786236+mshefer05@users.noreply.github.com>
2023-08-15 12:39:58 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function About() {
+  var newWindow = window.open("", "_blank");
+  newWindow.document.write(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>About</title>
+      <link rel="stylesheet" type="text/css" href="style.css">
+
+    </head>
+    <body>
+
+      
+    </body>
+    </html>
+  `);
+
+function contact() {
+  var newWindow = window.open("", "_blank");
+  newWindow.document.write(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>About</title>
+      <link rel="stylesheet" type="text/css" href="style.css">
+
+    </head>
+    <body>
+
+      
+    </body>
+    </html>
+  `);
